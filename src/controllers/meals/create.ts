e10be0cf5813fbb30e282a57c127985e6c36d917
Hideyuki <dev.hideyukitakahashi@gmail.com>
2026-06-17 01:09:10 -0300

    feat(meals): extract validation schema to external file

--- src/controllers/meals/create.ts
+++ src/controllers/meals/create.ts
@@ -1,21 +1,10 @@
-import { z } from 'zod';
 import { randomUUID } from 'crypto';
 import { FastifyRequest, FastifyReply } from 'fastify';
 import { knexConfig as knex } from '@/database';
+import { mealSchema } from '@/schemas/meal-schema';
 
 export async function create(request: FastifyRequest, reply: FastifyReply) {
-  const bodySchema = z.object({
-    name: z
-      .string({ error: 'O nome é obrigatório' })
-      .nonempty({ error: 'O nome não pode estar vazio' }),
-    description: z
-      .string({ error: 'A descrição é obrigatória' })
-      .nonempty({ error: 'A descrição não pode estar vazio' }),
-    date: z.coerce.date({ error: 'Data inválida' }),
-    within_the_diet: z.boolean({ error: 'Informe se está na dieta' }),
-  });
-
-  const body = bodySchema.parse(request.body);
+  const body = mealSchema.parse(request.body);
 
   let sessionId = request.cookies.sessionId;
 
